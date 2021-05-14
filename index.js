d82a4cb4b574f0a44ca70830170a28012b3d0206
joyfuldev <wings2132@naver.com>
2021-05-14 20:45:55 +0900

    Create

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hi");
+const express = require('express');
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT
+
+function handleListening() {
+    console.log(`Listening on: http://localhost:${PORT}`)
+}
+
+app.listen(5000, handleListening);
