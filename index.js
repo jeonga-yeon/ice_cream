0c7c82939e3c2439b657f9fce4b28f240dcd523e
joyfuldev <wings2132@naver.com>
2021-05-18 23:10:30 +0900

    Development Environment

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const express = require('express');
+import express from "express";
 const app = express();
 
 require("dotenv").config();
 const PORT = process.env.PORT
 
-function handleListening() {
-    console.log(`Listening on: http://localhost:${PORT}`)
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send('My Home');
+
+const handleProfile = (req, res) => res.send("My Profile");
+
+const between = (req, res, next) => {
+    console.log("between");
+    next();
 }
+
+app.use(between);
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
 
 app.listen(5000, handleListening);
