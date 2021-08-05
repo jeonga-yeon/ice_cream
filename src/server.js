c8e26236db0571f131d9266637929aced88057d9
joyfuldev <wings2132@naver.com>
2021-08-05 22:54:40 +0900

    webpack

--- src/server.js
+++ src/server.js
@@ -27,6 +27,7 @@ app.use(
 
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
+app.use("/assets", express.static("assets"));
 app.use("/", globalRouter);
 app.use("/posts", postRouter);
 app.use("/users", userRouter);
