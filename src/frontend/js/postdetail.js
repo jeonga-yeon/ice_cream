0a8bd1e732e3aad0aba7d8a00af1a991900ce0d5
joyfuldev <wings2132@naver.com>
2021-10-17 14:12:27 +0900

    next prev button

--- src/frontend/js/postdetail.js
+++ src/frontend/js/postdetail.js
@@ -1,9 +1,15 @@
 const container = document.querySelector(".container");
-const slide = document.querySelectorAll(".slide");
+const slide = document.querySelector(".slide");
 const images = document.querySelectorAll("img");
-const videos = document.querySelectorAll("video");
 
-console.log(videos);
+function paintBtn() {
+    const prev = document.createElement("i");
+    prev.className = "fas fa-chevron-left";
+    const next = document.createElement("i");
+    next.className = "fas fa-chevron-right";
+    container.appendChild(prev);
+    container.appendChild(next);
+}
 
 function imgResizing () {
     for(let i = 0; i < images.length; i++) {
@@ -30,36 +36,11 @@ function imgResizing () {
     }
 }
 
-function videoResizing() {
-    for(let i = 0; i < videos.length; i++) {
-        const width = videos[i].videoWidth;
-        const height = videos[i].videoHeight;
-
-        const MAX_WIDTH = 600;
-        const MAX_HEIGHT = 600;
-
-        if (width > height) {
-            if (width > MAX_WIDTH) {
-                height *= MAX_WIDTH / width;
-                width = MAX_WIDTH;
-            }
-        } else {
-            if (height > MAX_HEIGHT) {
-                width *= MAX_HEIGHT / height;
-                height = MAX_HEIGHT;
-            }
-        }
-
-        videos[i].videoWidth = width;
-        videos[i].videoHeight = height;
-    }
-}
-
 if(slide) {
     if(images) {
-        imgResizing();
-    }
-    if(videos) {
-        videoResizing();
+        window.onload = function () {
+            imgResizing();
+            paintBtn();
+        }
     }
 }
