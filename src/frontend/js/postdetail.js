279b0a30ad1d6b0ff874f7d7027db4940657c7d9
joyfuldev <wings2132@naver.com>
2021-10-18 23:56:57 +0900

    slide

--- src/frontend/js/postdetail.js
+++ src/frontend/js/postdetail.js
@@ -1,14 +1,45 @@
 const container = document.querySelector(".container");
 const slide = document.querySelector(".slide");
 const images = document.querySelectorAll("img");
+const videos = document.querySelectorAll("video");
+let curPosition = 0;
+let position = 0;
+
+function clickPrev(files, prev, next) {
+    if(curPosition > 0) {
+        next.removeAttribute("disabled");
+        position += files[0][curPosition].width;
+        files[0][curPosition].style.transform = `translateX(${position}px)`;
+        curPosition = curPosition - 1;
+    }
+    if(curPosition == 0) {
+        prev.setAttribute("disabled", "true");
+    }
+}
+
+function clickNext(files, prev, next) {
+    if(curPosition < files[0].length) {
+        prev.removeAttribute("disabled");
+        position -= files[0][curPosition].width;
+        files[0][curPosition].style.transform = `translateX(${position}px)`;
+        curPosition = curPosition + 1;
+    }
+    if(curPosition == files[0].length) {
+        next.setAttribute("disabled", "true");
+    }
+}
 
 function paintBtn() {
     const prev = document.createElement("i");
-    prev.className = "fas fa-chevron-left";
+    prev.className = "fas fa-chevron-left button prev";
     const next = document.createElement("i");
-    next.className = "fas fa-chevron-right";
+    next.className = "fas fa-chevron-right button next";
     container.appendChild(prev);
     container.appendChild(next);
+}
+
+function showSlides() {
+    
 }
 
 function imgResizing () {
@@ -30,17 +61,34 @@ function imgResizing () {
                 height = MAX_HEIGHT;
             }
         }
-
         images[i].width = width;
         images[i].height = height;
     }
 }
 
 if(slide) {
-    if(images) {
+    if(images || videos) {
         window.onload = function () {
             imgResizing();
             paintBtn();
+
+            const imgs = document.querySelectorAll("img");
+            const allVideos = document.querySelectorAll("video");
+            const files = [];
+            if(imgs) {
+                files.push(imgs);
+            }
+            if(allVideos) {
+                files.push(allVideos);
+            }
+
+            const prev = document.querySelector("i.prev");
+            const next = document.querySelector("i.next");
+
+            showSlides(files);
+
+            prev.addEventListener("click", function() {clickPrev(files, prev, next)});
+            next.addEventListener("click", function() {clickNext(files, prev, next)});
         }
     }
 }
