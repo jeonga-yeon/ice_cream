18d247afefd6f1f7ea8e9f3fb3fcfa81cc3a80c8
joyfuldev <wings2132@naver.com>
2022-01-05 01:38:20 +0900

    screens css

--- src/frontend/js/search.js
+++ src/frontend/js/search.js
@@ -2,5 +2,15 @@ const search = window.location.search;
 const params = new URLSearchParams(search);
 const keyword = params.get("keyword");
 const input = document.querySelector("input");
+const searchResults = document.querySelector(".search__results");
+const notFound = document.querySelector(".search__results .not_found");
 
 input.value = keyword
+
+if(!keyword) {
+    notFound.remove();
+    const span = document.createElement("span");
+    span.className = "not_found";
+    span.innerText = "검색어를 입력하세요."
+    searchResults.appendChild(span);
+}
