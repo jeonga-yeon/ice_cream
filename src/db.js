4bdb3f5dab2b01803f4c9c609bd9d3c045fb88cd
joyfuldev <wings2132@naver.com>
2022-02-25 22:44:55 +0900

    error resolution

--- src/db.js
+++ src/db.js
@@ -1,11 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGO_URL, { 
-    // useNewUrlParser: true, 
-    // useUnifiedTopology: true,
-    // useFindAndModify: false,
-    // useCreateIndex: true,
-});
+mongoose.connect(process.env.MONGO_URL);
 
 const db = mongoose.connection;
 
